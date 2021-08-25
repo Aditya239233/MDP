f9460b2f974f24f8df6d1d4eae21d78017eac2cb
Aditya239233 <aditya.chandrasekhar2001@gmail.com>
2021-08-25 23:32:22 +0800

    changed layout

--- Android/app/src/main/java/com/example/mdp_android/MainActivity.java
+++ Android/app/src/main/java/com/example/mdp_android/MainActivity.java
@@ -21,135 +21,26 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "Main Activity";
-    TextView showReceived;
-    EditText inputMessage;
-    Button sendButton;
-    ProgressDialog myDialog;
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-        Button button = findViewById(R.id.button1);
+        Button interactive_button = findViewById(R.id.button1);
+        Button bluetooth_button = findViewById(R.id.button2);
 
-        button.setOnClickListener(v -> openInteractiveControlView());
-        Button bluetoothButton = (Button) findViewById(R.id.bluetoothButton);
-        showReceived = findViewById(R.id.showReceived);
-        inputMessage = findViewById(R.id.inputMessage);
-        sendButton = findViewById((R.id.sendButton));
-        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, new IntentFilter("incomingMessage"));
-        bluetoothButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent popup = new Intent(MainActivity.this, BluetoothPopUp.class);
-                startActivity(popup);
-            }
-        });
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputMessage =  findViewById(R.id.inputMessage);
-                String message = inputMessage.getText().toString();
-                if (BluetoothConnectionService.BluetoothConnectionStatus == true) {
-                    byte[] bytes = message.getBytes(Charset.defaultCharset());
-                    BluetoothConnectionService.write(bytes);
-                }
-            }
-        });
-
-        myDialog = new ProgressDialog(MainActivity.this);
-        myDialog.setMessage("Waiting for other device to reconnect...");
-        myDialog.setCancelable(false);
-        myDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-    }
-    private BroadcastReceiver connectionWatcher2 = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            BluetoothDevice mDevice = intent.getParcelableExtra("Device");
-            String status = intent.getStringExtra("Status");
-            Log.d(TAG, "I'm in MAIN ACTIVITY");
-            if(status.equals("connected")){
-                try {
-                    myDialog.dismiss();
-                } catch(NullPointerException e){
-                    e.printStackTrace();
-                }
-                if (sharedPreferences.contains("connStatus"))
-                {
-                    editor = sharedPreferences.edit();
-                    editor.putString("connStatus", "Connected to " + mDevice.getName());
-                    editor.commit();
-                }
-                Log.d(TAG, "connectionWatcher2: Device now connected to "+mDevice.getName());
-//                Toast.makeText(MainActivity.this, "Device now connected to "+mDevice.getName(), Toast.LENGTH_LONG).show();
-
-            }
-            else if(status.equals("disconnected")){
-                sharedPreferences = getApplicationContext().getSharedPreferences("Shared Preferences", Context.MODE_PRIVATE);
-                if (sharedPreferences.contains("connStatus"))
-                {
-                    editor = sharedPreferences.edit();
-                    editor.putString("connStatus", "Disconnected");
-                    editor.commit();
-                }
-                Log.d(TAG, "connectionWatcher2: Disconnected from "+mDevice.getName());
-//                Toast.makeText(MainActivity.this, "Disconnected from "+mDevice.getName(), Toast.LENGTH_LONG).show();
-
-                myDialog.show();
-            }
-        }
-    };
-
-    BroadcastReceiver messageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String message = intent.getStringExtra("receivedMessage");
-            showReceived.setText(message);
-        }
-    };
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        try{
-            LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver);
-            LocalBroadcastManager.getInstance(this).unregisterReceiver(connectionWatcher2);
-        } catch(IllegalArgumentException e){
-            e.printStackTrace();
-        }
+        interactive_button.setOnClickListener(v -> openInteractiveControlView());
+        bluetooth_button.setOnClickListener(v -> openBluetoothView());
     }
 
-    @Override
-    protected void onPause(){
-        super.onPause();
-        try{
-            LocalBroadcastManager.getInstance(this).unregisterReceiver(connectionWatcher2);
-        } catch(IllegalArgumentException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        try{
-            IntentFilter filter2 = new IntentFilter("ConnectionStatus");
-            LocalBroadcastManager.getInstance(this).registerReceiver(connectionWatcher2, filter2);
-        } catch(IllegalArgumentException e){
-            e.printStackTrace();
-        }
-    }
 
     public void openInteractiveControlView() {
         Intent intent = new Intent(this, interactive_control.class);
+        startActivity(intent);
+    }
+
+    public void openBluetoothView() {
+        Intent intent = new Intent(this, bluetooth_home.class);
         startActivity(intent);
     }
 }
