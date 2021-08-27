62e1af9a782c85b2812760283d79386b75d47daa
cywk <yongwankang@hotmail.com>
2021-08-27 12:04:13 +0800

    implement interactive buttons

--- Android/app/src/main/java/com/example/mdp_android/interactive_control.java
+++ Android/app/src/main/java/com/example/mdp_android/interactive_control.java
@@ -1,9 +1,12 @@
 package com.example.mdp_android;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.nio.charset.Charset;
 
 public class interactive_control extends AppCompatActivity {
 
@@ -15,6 +18,52 @@ public class interactive_control extends AppCompatActivity {
         Button south_button = findViewById(R.id.button5);
         Button west_button = findViewById(R.id.button6);
         Button east_button = findViewById(R.id.button7);
+
+        north_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Go North";
+                if (BluetoothConnectionService.BluetoothConnectionStatus == true) {
+                    byte[] bytes = message.getBytes(Charset.defaultCharset());
+                    BluetoothConnectionService.write(bytes);
+                }
+            }
+        });
+
+        south_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Go South";
+                if (BluetoothConnectionService.BluetoothConnectionStatus == true) {
+                    byte[] bytes = message.getBytes(Charset.defaultCharset());
+                    BluetoothConnectionService.write(bytes);
+                }
+            }
+        });
+
+        west_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Go West";
+                if (BluetoothConnectionService.BluetoothConnectionStatus == true) {
+                    byte[] bytes = message.getBytes(Charset.defaultCharset());
+                    BluetoothConnectionService.write(bytes);
+                }
+            }
+        });
+
+        east_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Go East";
+                if (BluetoothConnectionService.BluetoothConnectionStatus == true) {
+                    byte[] bytes = message.getBytes(Charset.defaultCharset());
+                    BluetoothConnectionService.write(bytes);
+                }
+            }
+        });
+
     }
+
 
 }
