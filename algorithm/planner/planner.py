e04119c9c1e42820830e256d8c3cc069a91cc5de
Bairi Sahitya <bairisahitya7@gmail.com>
2021-09-13 23:53:42 +0800

    included api function in planner.py

--- algorithm/planner/planner.py
+++ algorithm/planner/planner.py
@@ -2,6 +2,10 @@ from translate import translate_tour
 from HybridAstarPlanner.solver import solve
 from HybridAstarPlanner.utils import Angle
 from test.planner_test import PlannerTest
+from flask import *
+import json, time, ast
+
+app = Flask(__name__)
 
 import pickle
 class Planner:
@@ -41,29 +45,93 @@ class Planner:
 
     def get_job(self, job_id):
         return self.cache[job_id]
+    def get_path(self, job_id):
+        path = []
+        for i in self.cache[job_id]:
+            for j in range(len(i.x)):
+                lst = []
+                lst.append(i.x[j])
+                lst.append(i.y[j])
+                lst.append(i.yaw[j]) 
+                path.append(lst)
+            path.append([-1,-1,-1])
+                
+        return path
 
 
-obstacle = [(32, 31, Angle.TWO_SEVENTY_DEG),
-(18, 16, Angle.ONE_EIGHTY_DEG),
-(9, 33, Angle.ZERO_DEG),
-(17, 24, Angle.ZERO_DEG),
-(28, 21, Angle.TWO_SEVENTY_DEG)]
+# api function
+@app.route('/get-path/', methods=['GET'])
+def get_path_coor():
+    obstacles = str(request.args.get('obstacles')) #/get-path/?obstacles=[10,20,etc]
+    obstacles = ast.literal_eval(obstacles)
+    print(obstacles)
 
-# Example
-p = Planner()
-p.set_obstacles(obstacle)
-job_id = p.run_job()
-intructions = p.get_instructions(job_id)
-print(intructions)
+    angleDict = {
+        'N': Angle.NINETY_DEG,
+        'E': Angle.ZERO_DEG,
+        'S': Angle.TWO_SEVENTY_DEG,
+        'W': Angle.ONE_EIGHTY_DEG
+    }
 
-tour = p.get_job(0)
-# from HybridAstarPlanner.solver import simulate
-# simulate(p.get_job(0), obstacle, save_gif=True, gif_name="./results/gif/fourth.gif")
-for path in tour:
-    for i in range(len(path.x)):
-        print(f"{path.x[i] :.3f}, {path.y[i] :.3f}, {path.direction[i]}, {path.yaw[i] :.3f}, {path.steer[i] :.3f}")
+    for i in range(len(obstacles)):
+        curr = obstacles[i]
+        print(curr)
+        obstacles[i] = (curr[0], curr[1],angleDict[curr[2]] )
+        
+    print(obstacles)
+    p = Planner()
+    # obs = get_obstacles()
+    p.set_obstacles(obstacles)
+    p.run_job()
+    data = {'status': "success", "data": p.get_path(0)}
+    json_dump = json.dumps(data)
+    return Response(json_dump, mimetype='application/json; charset=utf-8')
 
-    print("\n\n")
+if __name__ == '__main__':
+    app.run(port=3000)
+
+
+# obstacle = [(32, 31, Angle.TWO_SEVENTY_DEG),
+# (18, 16, Angle.ONE_EIGHTY_DEG),
+# (9, 33, Angle.ZERO_DEG),
+# (17, 24, Angle.ZERO_DEG),
+# (28, 21, Angle.TWO_SEVENTY_DEG)]
+
+# # Example
+# p = Planner()
+# p.set_obstacles(obstacle)
+# job_id = p.run_job()
+# intructions = p.get_instructions(job_id)
+# print(intructions)
+
+# tour = p.get_job(0)
+# # from HybridAstarPlanner.solver import simulate
+# # simulate(p.get_job(0), obstacle, save_gif=True, gif_name="./results/gif/fourth.gif")
+# for path in tour:
+#     for i in range(len(path.x)):
+#         print(f"{path.x[i] :.3f}, {path.y[i] :.3f}, {path.direction[i]}, {path.yaw[i] :.3f}, {path.steer[i] :.3f}")
+
+#     print("\n\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # for short testing (not used in production)
