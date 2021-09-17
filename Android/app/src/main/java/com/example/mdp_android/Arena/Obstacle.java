61c25e19cd4ca649c511eb8fc9d1a961871b08c1
Aditya239233 <aditya.chandrasekhar2001@gmail.com>
2021-09-17 08:43:02 +0800

    added arena

---
+++ Android/app/src/main/java/com/example/mdp_android/Arena/Obstacle.java
@@ -0,0 +1,186 @@
+package com.example.mdp_android.Arena;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+
+public class Obstacle {
+    float x, y;
+    float initX, initY;
+    float xArena, yArena = -1;
+    int touchCount = 0;
+    boolean actionDown = false;
+    String obsFace = "None";
+    String obsID, targetID;
+    Paint obsPaint;
+
+    public Obstacle(float x, float y, float initX, float initY, String obsID, int touchCount, String obsFace, Paint obsPaint, String targetID){
+        this.x = x;
+        this.y = y;
+        this.touchCount = touchCount;
+        this.obsFace = obsFace;
+        this.obsID = obsID;
+        this.obsPaint = obsPaint;
+        this.targetID = targetID;
+        this.initX = initX;
+        this.initY = initY;
+    }
+
+    public float getObsX(){
+        return  x;
+    }
+
+    public float getObsY(){
+        return  y;
+    }
+
+    public float [] getInitCoords () {
+        return new float [] {initX,initY};}
+
+    public String getObsFace(){
+        return obsFace;
+    }
+
+    public String getObsID(){
+        return obsID;
+    }
+
+    public int getTouchCount(){
+        return touchCount;
+    }
+
+    public int incrTouchCount(){
+        touchCount++;
+        return touchCount;
+    }
+
+    public float setObsX(float x){
+        this.x = x;
+        return x;
+    }
+
+    public float setObsY(float y){
+        this.y = y;
+        return y;
+    }
+
+    public void setInitX(float initX){
+        this.initX = initX;
+    }
+
+    public void setInitY(float initY){
+        this.initY = initY;
+    }
+
+    public void setInitCoords (float initX, float initY) {
+        this.initX = initX;
+        this.initY = initY;
+    }
+
+    public String setObsFace(int touchCount){
+        switch (touchCount){
+            case 1:
+                //Green: Left (West)
+                obsFace = "West";
+                break;
+            case 2:
+                //Red: Right
+                obsFace = "East";
+                break;
+            case 3:
+                //Yellow: Down
+                obsFace = "South";
+                break;
+            case 4:
+                //Blue: Front
+                obsFace = "North";
+                break;
+            default:
+                //Black
+                obsFace = " ";
+        }
+        this.obsFace = obsFace;
+        return obsFace;
+    }
+
+    public String setObsID(String obsID){
+        this.obsID = obsID;
+        return obsID;
+    }
+
+    //Setting action down status
+    public void setActionDown(boolean status){
+        //When touched down
+        this.actionDown = status;
+    }
+
+    //Getting action down status
+    public boolean getActionDown(){
+        return actionDown;
+    }
+
+    //To set new position of draggable object
+    public void setPosition(float x, float y){
+        this.x = (float) (x-12.5);
+        this.y = (float) (y-12.5);
+    }
+
+    //Use this for draggable object
+    public boolean isTouched(float x, float y){
+        Log.d("isTouched", x + ", " + y);
+        Log.d("isTouched", this.x + ", " + this.y);
+
+        boolean xIsInside = x > this.x && x < this.x + 100;
+        boolean yIsInside = y > this.y && y < this.y + 100;
+
+        Log.d("isTouched", xIsInside + ", " + yIsInside);
+
+        return xIsInside && yIsInside;
+    }
+
+    public void setObsMapCoord (float xArena, float yArena){
+        this.xArena = xArena;
+        this.yArena = yArena;
+    }
+
+    public float[] getObsMapCoord (){
+        return new float[]{xArena, yArena};
+    }
+
+    public int resetTouchCount() {
+        touchCount = 0;
+        return touchCount;
+    }
+
+    public Paint getObsPaint() {
+        return obsPaint;
+    }
+
+    //Setting the obstacle's paint settings
+    public Paint setObsPaint(Paint obsPaint) {
+        this.obsPaint = obsPaint;
+        return obsPaint;
+    }
+
+    public int setTouchCount(int touchCount) {
+        this.touchCount = touchCount%4;
+        return this.touchCount;
+    }
+
+    public String setTargetID(String targetID) {
+        this.targetID = targetID;
+        return targetID;
+    }
+
+    public String getTargetID() {
+        return targetID;
+    }
+
+    public void drawObj(Canvas canvas, Paint obstaclePaint){
+        //Log.d(TAG, "drawObj: Drawing Object");
+        Paint myPaint = new Paint();
+        myPaint.setColor(Color.BLACK);
+        canvas.drawRect(x,y,x+31,y+31,obstaclePaint);
+    }
+}
--- Android/app/src/main/java/com/example/mdp_android/arena_map.java
+++ Android/app/src/main/java/com/example/mdp_android/arena_map.java
@@ -1,124 +1,253 @@
 package com.example.mdp_android;
 
-import android.graphics.drawable.Drawable;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.DragEvent;
-import android.view.GestureDetector;
-import android.view.ViewGroup;
-import android.widget.GridView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
-import java.util.ArrayList;
+import com.example.mdp_android.Arena.Arena;
 
-import android.content.ClipData;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.DragShadowBuilder;
-import android.view.View.OnTouchListener;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+
+import java.nio.charset.Charset;
+
+/** Main Activity:
+ *  Will receive details like messages from chat, sending messages to RPI
+ *  connStatusTextView, BT status will be shared onto MainActivity and across the whole app
+ *  through sharedPreferences
+ */
 
 public class arena_map extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
-    GridView arena;
-    private static final String TAG = "I'm being called!";
-    private GestureDetector gestureDetector;
+    // Declare Variables
+    private static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor editor;
+    private static Context context;
+
+    private static Arena arenaMap;
+    static TextView txtRobotDirection, txtRobotCoord;
+    ProgressDialog myDialog;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
+
+        //Initialization
         super.onCreate(savedInstanceState);
         setContentView(R.layout.arena_map);
-        gestureDetector = new GestureDetector(this, new SingleTapConfirm());
 
-        // Define Arena
-        arena = findViewById(R.id.idGrid);
-//        arena.setOnDragListener(new MyDragListener());
+        //Get broadcasted msg
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, new IntentFilter("incomingMessage"));
 
-        ArrayList<Arena> courseModelArrayList = new ArrayList<Arena>();
+        //sharedPreferences set up
+        arena_map.context = getApplicationContext();
+        this.sharedPreferences();
+        editor.putString("message", "");
+        editor.putString("direction","None");
+        editor.putString("connStatus", "Disconnected");
+        editor.commit();
 
-        for (int i = 0; i < 100; i++)
-            courseModelArrayList.add(new Arena(R.drawable.border_black_background));
+        //Arena map
+        arenaMap = new Arena(this);
+        arenaMap = findViewById(R.id.mapView);
+        txtRobotCoord = findViewById(R.id.txtRobotPosition);
+        txtRobotDirection = findViewById(R.id.txtRobotDirection);
 
-        ArenaAdapter adapter = new ArenaAdapter(this, courseModelArrayList);
-        arena.setAdapter(adapter);
-
-        // Define Block
-        ImageView obstacle = findViewById(R.id.myimage1);
-        obstacle.setOnTouchListener(new MyTouchListener());
-        obstacle.setOnLongClickListener(new View.OnLongClickListener() {
-
+        //Process Dialog
+        myDialog = new ProgressDialog(arena_map.this);
+        myDialog.setMessage("Waiting for other device to reconnect...");
+        myDialog.setCancelable(false);
+        myDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
             @Override
-            public boolean onLongClick(View view) {
-                ClipData data = ClipData.newPlainText("", "");
-                DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
-                        view);
-                view.startDrag(data, shadowBuilder, view, 0);
-                return true;
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        Button set_robot = findViewById(R.id.button10);
+        Button reset_map = findViewById(R.id.button11);
+
+        reset_map.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Arena tempArenaMap = arena_map.getArenaMap();
+                tempArenaMap.resetArena();
+            }
+        });
+
+        set_robot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Arena tempArenaMap = arena_map.getArenaMap();
+                tempArenaMap.setStartingPoint(true);
             }
         });
     }
 
-    private final class MyTouchListener implements OnTouchListener {
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (gestureDetector.onTouchEvent(motionEvent)) {
-                view.setRotation(view.getRotation() + 90);
-                Log.d(TAG, "rotation : " + view.getRotation());
-                return false;
-            }
-//            else if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-//                ClipData data = ClipData.newPlainText("", "");
-//                DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
-//                        view);
-//                view.startDrag(data, shadowBuilder, view, 0);
-//                return true;
-//            }
-            return false;
-        }
+
+    private static void showLog(String message) {
+        Log.d(TAG, message);
     }
 
-    private class SingleTapConfirm extends GestureDetector.SimpleOnGestureListener {
+    public static void sharedPreferences() {
+        sharedPreferences = arena_map.getSharedPreferences(arena_map.context);
+        editor = sharedPreferences.edit();
+    }
+
+    private static SharedPreferences getSharedPreferences(Context context){
+        return context.getSharedPreferences("Shared Preferences", Context.MODE_PRIVATE);
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        switch (requestCode){
+//            case 1:
+//                if(resultCode == Activity.RESULT_OK){
+//                    mBTDevice = (BluetoothDevice) data.getExtras().getParcelable("mBTDevice");
+//                    myUUID = (UUID) data.getSerializableExtra("myUUID");
+//                }
+//        }
+//    }
+
+    // Send message to bluetooth
+    public static void printMessage(String message) {
+        showLog("Entering printMessage");
+        editor = sharedPreferences.edit();
+
+        if (BluetoothConnectionService.BluetoothConnectionStatus == true) {
+            byte[] bytes = message.getBytes(Charset.defaultCharset());
+            BluetoothConnectionService.write(bytes);
+        }
+        showLog(message);
+//        editor.putString("message", ChatFragment.getMessageReceivedTextView().getText() + "\n" + message);
+        editor.commit();
+        refreshMessageReceived();
+    }
+
+    public static void refreshMessageReceived() {
+        String received = sharedPreferences.getString("message", "");
+//        ChatFragment.getMessageReceivedTextView().setText(sharedPreferences.getString("message", ""));
+    }
+
+    private BroadcastReceiver mBroadcastReceiver5 = new BroadcastReceiver() {
         @Override
-        public boolean onSingleTapUp(MotionEvent event) {
-            return true;
-        }
-    }
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG,"BroadcastReceiver 5: ");
 
-    class MyDragListener implements View.OnDragListener {
-        Drawable enterShape = getResources().getDrawable(
-                R.drawable.border_black_background);
-        Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+            BluetoothDevice mDevice = intent.getParcelableExtra("Device");
+            String status = intent.getStringExtra("Status");
+            sharedPreferences();
 
-        @Override
-        public boolean onDrag(View v, DragEvent event) {
-            int action = event.getAction();
-            switch (event.getAction()) {
-                case DragEvent.ACTION_DRAG_STARTED:
-                    // do nothing
-                    break;
-                case DragEvent.ACTION_DRAG_ENTERED:
-                    v.setBackgroundDrawable(enterShape);
-                    break;
-                case DragEvent.ACTION_DRAG_EXITED:
-                    v.setBackgroundDrawable(normalShape);
-                    break;
-                case DragEvent.ACTION_DROP:
-                    // Dropped, reassign View to ViewGroup
-                    View view = (View) event.getLocalState();
-                    ViewGroup owner = (ViewGroup) view.getParent();
-                    owner.removeView(view);
-                    LinearLayout container = (LinearLayout) v;
-                    container.addView(view);
-                    view.setVisibility(View.VISIBLE);
-                    break;
-                case DragEvent.ACTION_DRAG_ENDED:
-                    v.setBackgroundDrawable(normalShape);
-                default:
-                    break;
+            if(status.equals("connected")){
+                try {
+                    myDialog.dismiss();
+                } catch(NullPointerException e){
+                    e.printStackTrace();
+                }
+
+                Log.d(TAG, "mBroadcastReceiver5: Successfully connected to "+mDevice.getName());
+                Toast.makeText(arena_map.this, "Successfully connected to "+mDevice.getName(), Toast.LENGTH_LONG).show();
+                editor.putString("connStatus", "Connected to " + mDevice.getName());
+
             }
-            return true;
+            else if(status.equals("disconnected")){
+                Log.d(TAG, "mBroadcastReceiver5: Disconnected from "+mDevice.getName());
+                Toast.makeText(arena_map.this, "Disconnected from "+mDevice.getName(), Toast.LENGTH_LONG).show();
+                editor.putString("connStatus", "Disconnected");
+                myDialog.show();
+            }
+            editor.commit();
+        }
+    };
+
+    BroadcastReceiver messageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message = intent.getStringExtra("theMessage");
+            showLog("Message Received: " + message);
+
+            arenaMap.updateMap(message);
+
+            sharedPreferences();
+            String receivedText = sharedPreferences.getString("message", "") + "\n" + message;
+            editor.putString("message", receivedText);
+            editor.commit();
+
+            refreshMessageReceived();
+        }
+    };
+
+    public static Arena getArenaMap(){
+        return arenaMap;
+    }
+
+    public static void setRobotDetails(int x, int y, String direction){
+        Log.d(TAG, "setRobotDetails: Getting current robot coordinates");
+
+        if (x == -1 && y == -1) {
+            txtRobotCoord.setVisibility(View.INVISIBLE);
+            txtRobotDirection.setVisibility(View.INVISIBLE);
+        } else {
+            txtRobotCoord.setText(String.valueOf(x) + "," +
+                    String.valueOf(y));
+            txtRobotDirection.setText(direction);
         }
     }
 
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        try{
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver);
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver5);
+        } catch(IllegalArgumentException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        try{
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver5);
+        } catch(IllegalArgumentException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        try{
+            IntentFilter filter2 = new IntentFilter("ConnectionStatus");
+            LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver5, filter2);
+        } catch(IllegalArgumentException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        showLog("Entering onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+
+        outState.putString(TAG, "onSaveInstanceState");
+        showLog("Exiting onSaveInstanceState");
+    }
 }
